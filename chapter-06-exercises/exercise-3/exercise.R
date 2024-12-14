2222983514844e0ffbe8fab807afc139f2132ca6
Stanislaw <stkrzesniak@gmail.com>
2024-12-14 17:05:25 +0100

    chapter 5 and 6

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,17 +2,24 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
+add_three <- function(x){
+  out <- x + 3
+  out
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
 
-
+ten <- add_three(7)
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(num_of_feet, num_of_inches){
+  meters <-  0.3048 * num_of_feet + 0.0254 * num_of_inches
+  #meters
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
+height_in_meters <- imperial_to_metric(6, 5)
 
--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -5,11 +5,17 @@
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
 
-
+is_twice_as_long <- function(str1, str2) {
+  min_len <- min(nchar(str1), nchar(str2))
+  dif <- abs(nchar(str1)-nchar(str2))
+  twice <- dif/min_len > 2
+}
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
+(is_twice_as_long("abba", "c"))
+(is_twice_as_long( "c", "abba"))
+(is_twice_as_long( "cabas", "abba"))
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -17,8 +23,23 @@
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-
+describe_difference <- function(str1, str2) {
+  len1 <- nchar(str1)
+  len2 <- nchar(str2)
+  if (len1 > len2){
+    N <- len1 - len2
+    return(paste("Your first string is longer by", N, "characters"))
+  } else if (len1 < len2) {
+    N <- len2 - len1
+    return(paste("Your second string is longer by", N, "characters"))
+  } else {
+    return("Your strings are the same length!")
+  }
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
+(describe_difference("ass", "abs"))
+(describe_difference("abss", "abs"))
+(describe_difference("ass", "abbbs"))
 

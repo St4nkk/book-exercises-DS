4990863908b82878295e3f7201a98728bc6f6828
Stanislaw <stkrzesniak@gmail.com>
2024-12-16 20:18:18 +0100

    ch12-13-14

--- chapter-13-exercises/exercise-1/exercise.R
+++ chapter-13-exercises/exercise-1/exercise.R
@@ -2,34 +2,34 @@
 
 # Install and load the `dplyr`, `DBI`, and `RSQLite` packages for accessing
 # databases
-
+pacman::p_load(dplyr, DBI, RSQLite)
 
 # Create a connection to the `Chinook_Sqlite.sqlite` file in the `data` folder
 # Be sure to set your working directory!
-
+db_conn <- dbConnect(SQLite(), dbname = "data/Chinook_Sqlite.sqlite")
 
 # Use the `dbListTables()` function (passing in the connection) to get a list
 # of tables in the database.
-
+dbListTables(db_conn)
 
 # Use the `tbl()`function to create a reference to the table of music genres.
 # Print out the the table to confirm that you've accessed it.
-
+genre <- tbl(db_conn, "Genre")
 
 # Try to use `View()` to see the contents of the table. What happened?
-
+View(genre)
 
 # Use the `collect()` function to actually load the genre table into memory
 # as a data frame. View that data frame.
-
+genre_df <- collect(genre)
 
 # Use dplyr's `count()` function to see how many rows are in the genre table
-
+rows <- count(genre_df)
 
 
 # Use the `tbl()` function to create a reference the table with track data.
 # Print out the the table to confirm that you've accessed it.
-
+track <- tbl(db_conn, "Track")
 
 # Use dplyr functions to query for a list of artists in descending order by
 # popularity in the database (e.g., the artist with the most tracks at the top)
@@ -37,19 +37,37 @@
 #   group rows by the artist and count them. Finally, arrange the results.
 # - Use pipes to do this all as one statement without collecting the data into
 #   memory!
-
+track %>% 
+  filter(!is.na(Composer)) %>% 
+  group_by(Composer) %>%
+  summarise(tracks_num = count(TrackId)) %>% 
+  arrange(desc(tracks_num)) 
 
 # Use dplyr functions to query for the most popular _genre_ in the library.
 # You will need to count the number of occurrences of each genre, and join the
 # two tables together in order to also access the genre name.
 # Collect the resulting data into memory in order to access the specific row of
 # interest
+genre_by_tracks <- collect(
+  track %>% 
+  filter(!is.na(GenreId)) %>% 
+  group_by(GenreId) %>% 
+  summarise(num_of_tracks = count(TrackId)) %>% 
+  left_join(genre, by="GenreId") %>% 
+  arrange(desc(num_of_tracks)) )
 
-
+most_popular_genre <- top_n(genre_by_tracks, n=1, num_of_tracks)
 # Bonus: Query for a list of the most popular artist for each genre in the
 # library (a "representative" artist for each).
 # Consider using multiple grouping operations. Note that you can only filter
 # for a `max()` value if you've collected the data into memory.
-
+most_popular_artist_by_genre <- collect(
+  track %>% 
+  filter(!is.na(GenreId), !is.na(Composer)) %>% 
+  group_by(GenreId, Composer) %>%
+  summarise(num_of_track = count(TrackId)) %>% 
+  filter(num_of_track == max(num_of_track, na.rm = T)) %>% 
+  left_join(genre, by="GenreId")  )
 
 # Remember to disconnect from the database once you are done with it!
+dbDisconnect(db_conn)
--- chapter-14-exercises/exercise-2/exercise.R
+++ chapter-14-exercises/exercise-2/exercise.R
@@ -9,10 +9,10 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikeys.R")
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+book_name <- "Waiting+for+Godot"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -23,25 +23,35 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
-
+query_params <- list(
+  title = book_name,
+  'api-key' = nty_apikey
+)
+response <- GET("https://api.nytimes.com/svc/books/v3/reviews.json", query = query_params)
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+text_content <- content(response, "text")
+book_review <- fromJSON(text_content)
 
 # What kind of data structure did this produce? A data frame? A list?
-
+typeof(book_review)
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+str(book_review)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+godot_review <- book_review[["results"]]
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
-
+short_summary <- godot_review$summary[[1]]
+link_to_article <- godot_review$url[[1]]
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+info_list <- list(
+  'short summary' = short_summary,
+  'article url' = link_to_article)
+
+info_list

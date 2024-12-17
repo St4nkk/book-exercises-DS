88f7807bf2d700ca2f85461339eaf03d58d52445
Stanislaw <stkrzesniak@gmail.com>
2024-12-17 12:35:04 +0100

    ch17

--- chapter-17-exercises/exercise-1/exercise.R
+++ chapter-17-exercises/exercise-1/exercise.R
@@ -2,7 +2,7 @@
 # (using plotly)
 
 # Load necessary packages (`dplyr`, `ggplot2`, and `plotly`)
-
+pacman::p_load(dplyr, ggplot2, plotly)
 
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
@@ -10,7 +10,7 @@
 # Load the `"data/IHME_WASHINGTON_MORTALITY_RATES_1980_2014.csv` file
 # into a variable `mortality_rates`
 # Make sure strings are *not* read in as factors
-
+mortality_rates <- read.csv("data/IHME_WASHINGTON_MORTALITY_RATES_1980_2014.csv", stringsAsFactors = F)
 
 # This is actually a very large and rich dataset, but we will only focus on
 # a small subset of it. Create a new data frame `plot_data` by filtering the
@@ -20,16 +20,22 @@
 # - The `cause_name` is "Neoplasms"
 # - The `year_id` is greater than 2004
 # - Only keep the columns `sex`, `year_id`, and `mortality_rate`
-
+plot_data <- mortality_rates %>% 
+  filter(location_name == "King County", 
+         sex != "Both",
+         cause_name == "Neoplasms",
+         year_id > 2004) %>% 
+  select(sex, year_id, mortality_rate)
 
 # Using ggplot2 (recall chapter 16), make a grouped ("dodge") bar chart of
 # mortality rates each year, with different bars for each sex.
 # Store this plot in a variable `mort_plot`
-
-
+mort_plot <- ggplot(plot_data) +
+  geom_col(aes(x = year_id, y = mortality_rate, fill = sex), position = "dodge")
+mort_plot
 # To make this plot interactive, pass `mort_plot` to the `ggplotly()` function
 # (which is part of the `plotly` package). This will make your plot interactive!
-
+ggplotly(mort_plot)
 
 # As an alternative to making a ggplot chart interactive, we can build the same
 # plot using the plotly API directly
@@ -39,12 +45,21 @@
 # the y variable, and `sex` as the color variable. 
 # (make sure to specify these as *formulas*)
 # Also set the plot type to "bar". Store the result in a variable.
+plotly_bar <- plot_ly(plot_data,
+        x = ~year_id,
+        y = ~mortality_rate,
+        color = ~sex,
+        type = "bar")
 
-
-
+plotly_bar
 # You should see that the cancer mortaility rates for female and males 
 # each year are plotted next to each other.
 # Now that we have the foundation down, we can make our plot more presentable
 # by adding a layout to the plot. Add a title for the overall plot, and
 # a title for each axes.
-
+plotly_bar %>% 
+  layout(
+    title = "Umieralność spowodowana nowotworami w King County od roku 2004",
+    xaxis = list(title = "Rok"),
+    yaxis = list(title = "Umieralność")
+  )
--- chapter-17-exercises/exercise-2/exercise.R
+++ chapter-17-exercises/exercise-2/exercise.R
@@ -2,15 +2,16 @@
 # (using rbokeh)
 
 # Load necessary packages (`dplyr`, `ggplot2`, and `rbokeh`)
-
-
+pacman::p_load(dplur, ggplot2)
+devtools::install_github("hafen/rbokeh")
+library("rbokeh")
 # Set your working directory using the RStudio menu:
 # Session > Set Working Directory > To Source File Location
 
 # Load the `"data/IHME_WASHINGTON_MORTALITY_RATES_1980_2014.csv` file
 # into a variable `mortality_rates`
 # Make sure strings are *not* read in as factors
-
+mortality_rates <- read.csv("data/IHME_WASHINGTON_MORTALITY_RATES_1980_2014.csv", stringsAsFactors = F)
 
 
 # This is actually a very large and rich dataset, but we will only focus on
@@ -21,7 +22,12 @@
 # - The `cause_name` is "Neoplasms"
 # - The `year_id` is greater than 2004
 # - Only keep the columns `sex`, `year_id`, and `mortality_rate`
-
+plot_data <- mortality_rates %>% 
+  filter(location_name == "King County",
+         sex != "Both",
+         cause_name == "Neoplasms",
+         year_id > 2004) %>% 
+  select(sex, year_id, mortality_rate)
 
 # Creating a plot with rbokeh requires a few steps:
 
@@ -29,7 +35,9 @@
 # - The `data`  to plot (e.g., `plot_data`), and
 # - The `title` (e.g., "Neoplasms Mortality Rate in King County")
 # Store this in a variable `p`
-
+p <- figure(
+  data = plot_data,
+  title = "Umieralność spowodowana nowotworami w King County od 2004 roku")
 
 # Then, add a layer of bars (via a pipe %>%) specifying your data encodings. 
 # You will use the `ly_bar()` function to do this, which (oddly) requires that 
@@ -38,7 +46,16 @@
 # - The `y` variable as the `mortality_rate`
 # - The `color` varaible as the `sex`
 # - The `position` as "dodge" (as opposed to stacked)
-
+p <- p %>%
+  ly_bar(
+    x = as.character(year_id), 
+    y = mortality_rate,        
+    color = sex,               
+    position = "dodge"         
+  )
 
 # Finally, you can add better axis labels (again, via a pipe %>%) by passing 
 # your plot `p` to the `x_axis()` and `y_axis()` functions.
+p %>% 
+  x_axis(label = "Year") %>% 
+  y_axis(label = "Mortality Rate")
